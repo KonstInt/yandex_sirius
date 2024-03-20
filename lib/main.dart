e7cccac7fd2c711389ab58dd07622227309c9235
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2024-03-20 23:38:17 +0300

    fix Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
