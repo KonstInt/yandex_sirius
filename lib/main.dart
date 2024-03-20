41fc7b195a3788c5a0f634d31302518518efe733
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2024-03-20 21:08:39 +0300

    add Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:yandex_sirius/app/app.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const App());
 }
