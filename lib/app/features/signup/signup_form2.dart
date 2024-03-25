e0268c1e814f39b28a46c56172c78aff344891e6
Vladislav Vechkanov <v.vechkanov@innopolis.university>
2024-03-25 14:43:03 +0300

    fix firebase configuration

--- lib/app/features/signup/signup_form2.dart
+++ lib/app/features/signup/signup_form2.dart
@@ -15,11 +15,8 @@ class SignUpForm2 extends StatelessWidget {
     return BlocListener<SignupCubit, SignupState>(
       listener: (context, state) {
         if (state.status.isSuccess) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              const SnackBar(content: Text('success')),
-            );
+          Navigator.pop(
+            context,);
         } else if (state.status.isFailure && state.errorMessage != null) {
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
